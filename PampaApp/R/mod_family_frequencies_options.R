dd5e5c37a9e68fa00753b5ff0c6a1c9956ec8474
Melanie Madelin <melanie.madelin@gmail.com>
2022-05-04 16:48:49 +0200

    Update family frequencies options

--- PampaApp/R/mod_family_frequencies_options.R
+++ PampaApp/R/mod_family_frequencies_options.R
@@ -20,20 +20,11 @@ mod_family_frequencies_options_server <- function(id){
     ns <- session$ns
 
     family_optn <- shiny::reactiveValues(
-      nb_obs = getOption("P.NbObs"),
-      nb_obs_col = getOption("P.NbObsCol"),
       warnings = getOption("P.warnings")
     )
 
     shiny::observeEvent(input$family_options, {
       shiny::showModal(shiny::modalDialog(
-        shiny::checkboxInput(ns("family_options_nb_obs"), "Display the number of stations per bars?",
-          value = getOption("P.NbObs")),
-        shiny::div(
-          colourpicker::colourInput(ns("family_options_nb_obs_col"), "Colours for station numbers:",
-            value = getOption("P.NbObsCol"), showColour = "background"),
-          style = "margin-left:25px;"
-        ),
         shiny::checkboxInput(ns("family_options_warnings"), "Display warnings for low numbers?",
           value = getOption("P.warnings")),
         title = "Options for family barplots",
@@ -47,23 +38,14 @@ mod_family_frequencies_options_server <- function(id){
     })
 
     shiny::observeEvent(input$family_options_ok,{
-      setOption("P.NbObs", input$family_options_nb_obs)
-      setOption("P.NbObsCol", input$family_options_nb_obs_col)
       setOption("P.warnings", input$family_options_warnings)
-
       shiny::removeModal()
     })
 
     shiny::observeEvent(input$family_options_reset, {
-      setOption("P.NbObs", family_optn$nb_obs)
-      setOption("P.NbObsCol", family_optn$nb_obs_col)
       setOption("P.warnings", family_optn$warnings)
-
-      shiny::updateCheckboxInput(inputId = "family_options_nb_obs", value = family_optn$nb_obs)
-      colourpicker::updateColourInput(session = session, inputId = "family_options_nb_obs_col", value = family_optn$nb_obs_col)
       shiny::updateCheckboxInput(inputId = "family_options_warnings", value = family_optn$warnings)
     })
-
   })
 }
 
